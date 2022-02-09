7ecb23482f7a3d88e49ea20d79e62ae0e008a446
Andrew <andreikin@mail.ru>
2022-02-09 13:16:44 +0300

    add maximize_restore, name verification and window scale setup

--- UI/MainWindow.py
+++ UI/MainWindow.py
@@ -3,11 +3,12 @@ import os
 import re
 import sys
 import tempfile
-from random import randint
 
-from PyQt5.QtCore import QVariantAnimation, QSettings, Qt
-from PyQt5.QtGui import QIcon, QStandardItemModel
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QWidget, QLabel
+from PyQt5 import QtGui, QtCore
+from PyQt5 import Qt
+from PyQt5.QtCore import  QSettings, QRegExp
+from PyQt5.QtGui import QIcon, QRegExpValidator
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QWidget, QLabel, QSizeGrip
 
 from UI.AssetWidget import AssetWidget
 from UI.CustomTitleBar import CustomTitleBar
@@ -19,11 +20,11 @@ from UI.TreeAssetsWidget import MenuTreeWidget
 from UI.Ui_MainWindow import Ui_MainWindow
 from UI.Ui_function import UiFunction
 from Utilities.Logging import logger
-from Utilities.Utilities import get_tags_from_path
-from settings import COLUMN_WIDTH, SPACING, START_WINDOW_SIZE, SFX
+from Utilities.Utilities import get_tags_from_path, convert_path_to_global
+from settings import COLUMN_WIDTH, SPACING, START_WINDOW_SIZE, SFX, FONT_SIZE
 
 
-class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
+class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar, ):
     def __init__(self, in_controller, parent=None):
         super(QMainWindow, self).__init__(parent)
 
@@ -35,8 +36,15 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
 
         self.Controller = in_controller
         self.setupUi(self)
-        self.drop_menu.setFixedWidth(0)
-        self.resize(*START_WINDOW_SIZE)
+
+        # window scale setup
+        sizegrip = QSizeGrip(self)
+        sizegrip.setStyleSheet("image: url(:/icons/icons//scale.png);")
+        sizegrip.setFixedSize(30, 20)
+        self.footer_layout.addWidget(sizegrip, 0,  QtCore.Qt.AlignBottom | QtCore.Qt.AlignRight)
+
+        # name verification
+        self.name_lineEdit.setValidator(QRegExpValidator(QRegExp("^[A-z0-9]+$")))
 
         # insert Gallery widget
         self.gallery = GalleryWidget(icons_width=COLUMN_WIDTH, spacing=SPACING)
@@ -66,7 +74,6 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
         self.search_lineEdit.returnPressed.connect(self.Controller.refresh_ui)
         self.add_dir_button.clicked.connect(self.tree_widget.add_directory)
         self.add_dir_line_edit.returnPressed.connect(self.tree_widget.add_directory)
-        self.name_lineEdit.textChanged.connect(self.edit_path_when_input_name)
         self.expand_button.clicked.connect(lambda: self.switch_pages(0))
         self.tree_button.clicked.connect(lambda: self.switch_pages(1))
         self.settings_button.clicked.connect(lambda: self.switch_pages(2))
@@ -82,15 +89,35 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
         self.db_path_button.clicked.connect(self.add_db_path_btn)
         self.btn_close.clicked.connect(self.close)
         self.btn_minimize.clicked.connect(self.showMinimized)
-        self.btn_maximize.clicked.connect(self.showMaximized)
+        self.btn_maximize.clicked.connect(self.maximize_restore)
+        self.font_spinBox.valueChanged.connect(self.set_font_size)
 
         self.asset_menu_mode = "Add"
+        self.maximize = False # application size state
+
         # object for save settings
-        file_path = os.path.join(tempfile.gettempdir(), 'asset_manager_settings.ini')
-        self.settings = QSettings(file_path, QSettings.IniFormat)
+        self.settings = None
         self.load_settings()
 
-        logger.debug("Ui loaded successfully.")
+        if not self.Controller.connect_db:
+            self.status_message("Problems connecting to the database.", state="ERROR")
+        logger.debug("Ui loaded successfully.\n")
+
+    def maximize_restore(self):
+        if self.maximize:
+            self.maximize = False
+            self.btn_maximize.setIcon(QIcon(":/icons/icons/square.svg"))
+            self.btn_maximize.setIconSize(QtCore.QSize(14, 14))
+            self.showNormal()
+        else:
+            self.showMaximized()
+            self.btn_maximize.setIcon(QIcon(":/icons/icons/normalise.png"))
+            self.btn_maximize.setIconSize(QtCore.QSize(20, 20))
+
+            self.maximize = True
+
+
+
 
     @property
     def asset_menu_mode(self):
@@ -161,30 +188,27 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
     def get_asset_data(self):
         # get from ui
         out_dict = dict()
-        out_dict['name'] = self.name_lineEdit.text().replace("_", " ")
-        out_dict['tags'] = re.findall(r'[0-9A-z_]+', self.tag_lineEdit.text())
-        logger.debug(self.path_lineEdit.text())
-        logger.debug("/"+out_dict['name'].replace("_", " ")+SFX)
-        if self.path_lineEdit.text() == "/"+out_dict['name']+SFX:
-            self.status_message("Need to select the directory in which the asset will be", state="ERROR")
-            return None
-
+        out_dict['name'] = self.name_lineEdit.text()
         out_dict['path'] = self.path_lineEdit.text()
-        out_dict['icon'] = self.image_lineEdit.text()
-        out_dict['description'] = self.description_textEdit.toPlainText()
 
-        folders_tags = get_tags_from_path(out_dict['path'])
-        out_dict['tags'] += folders_tags
-        out_dict['tags'] = list(set(out_dict['tags']))  # make tags unic
-
-        # get a list of scenes paths
-        out_dict['scenes'] = self.file_list_widget.get_list()
-
-        # get a list of gallery paths
-        out_dict['gallery'] = self.gallery_list_widget.get_list()
-
-        logger.debug(json.dumps(out_dict))
         if out_dict['name'] and out_dict['path']:
+            out_dict['path'] = convert_path_to_global(out_dict['path']) + "/" + out_dict['name'] + SFX
+            out_dict['tags'] = re.findall(r'[0-9A-z_]+', self.tag_lineEdit.text())
+            out_dict['icon'] = self.image_lineEdit.text()
+            out_dict['description'] = self.description_textEdit.toPlainText()
+
+            folders_tags = get_tags_from_path(out_dict['path'])
+            out_dict['tags'] += folders_tags
+            out_dict['tags'] = list(set(out_dict['tags']))  # make tags unic
+
+            # get a list of scenes paths
+            out_dict['scenes'] = self.file_list_widget.get_list()
+
+            # get a list of gallery paths
+            out_dict['gallery'] = self.gallery_list_widget.get_list()
+
+            logger.debug(json.dumps(out_dict))
+
             return out_dict
         else:
             self.status_message("One of the required fields is not filled", state="ERROR")
@@ -198,12 +222,28 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
         """
         If settings not exist - load default settings
         """
+        # set default settings
+        self.drop_menu.setFixedWidth(0)
+        self.resize(*START_WINDOW_SIZE)
+
+        file_path = os.path.join(tempfile.gettempdir(), 'asset_manager_settings.ini')
+        has_settings = os.path.exists(file_path)
+        self.settings = QSettings(file_path, QSettings.IniFormat)
+
         if self.settings.contains("ui settings"):
             self.search_lineEdit.setText(self.settings.value("ui settings"))
         if self.settings.contains("db settings"):
             self.db_path_lineEdit.setText(self.settings.value("db settings"))
         if self.settings.contains("ui pos"):
             self.move(self.settings.value("ui pos"))
+
+        if self.settings.contains("font size"):
+            self.font_spinBox.setValue(int(self.settings.value("font size")))
+        else:
+            self.font_spinBox.setValue(FONT_SIZE)
+
+        if has_settings:
+            logger.info("Settings loaded from file : " + file_path)
 
     def closeEvent(self, evt):
         """
@@ -212,17 +252,13 @@ class MainWindow(QMainWindow, Ui_MainWindow, UiFunction, CustomTitleBar,):
         self.settings.setValue("ui settings", self.search_lineEdit.text())
         self.settings.setValue("db settings", self.db_path_lineEdit.text())
         self.settings.setValue("ui pos", self.pos())
+        self.settings.setValue("font size", self.font_spinBox.value())
         logger.debug("Settings saved")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
-
-    for x in range(135):
-        but = QPushButton()
-        but.setFixedHeight(randint(50, 150))
-        window.gallery.add_widget(but)
 
     window.show()
     app.exec_()
