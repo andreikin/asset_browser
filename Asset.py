07868e551a389a2ab2192580ae9e1f9b3f0c3280
Andrey Belyaev <andreikin@mail.ru>
2022-02-19 23:04:15 +0300

    add name edit ability

--- Asset.py
+++ Asset.py
@@ -8,8 +8,8 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
 from Utilities.Logging import logger
-from Utilities.Utilities import get_library_path, get_preview_images
-from settings import INFO_FOLDER, CONTENT_FOLDER, GALLERY_FOLDER, ICON_WIDTH, DELETED_ASSET_FOLDER
+from Utilities.Utilities import get_library_path, get_preview_images, rename_path_list
+from settings import INFO_FOLDER, CONTENT_FOLDER, GALLERY_FOLDER, ICON_WIDTH, DELETED_ASSET_FOLDER, SFX
 
 
 class Asset:
@@ -19,40 +19,76 @@ class Asset:
     """
 
     def __init__(self, in_controller, **kwargs):
+
         self.Controller = in_controller
-        self.__icon = ""
+        self.asset_id = kwargs.setdefault("asset_id", None)
+        self.old_asset_data = dict()
+        if self.asset_id:
+            self.edit_mode = True
+            self.old_asset_data = self.get_old_asset_data()
+        else:
+            self.edit_mode = False
+
+        self.__name = None
         self.__path = None
+        self.__icon = None
 
         # folders for info and content
-        self.asset_id = self.Controller.Models.find_asset_by_name(kwargs["name"])
         self.asset_json = None
         self.asset_info_folder = None
         self.content_folder = None
         self.gallery_folder = None
 
+        self.scenes = kwargs.setdefault("scenes", None)
+        self.gallery = kwargs.setdefault("gallery", None)
         self.name = kwargs["name"]
         self.path = kwargs["path"]
+
         self.tags = kwargs.setdefault("tags", None)
         self.icon = kwargs.setdefault("icon", None)
         self.description = kwargs.setdefault("description", None)
-        self.scenes = kwargs.setdefault("scenes", None)
-        self.gallery = kwargs.setdefault("gallery", None)
 
         logger.debug(json.dumps(self.asset_data()))
 
-    @staticmethod
-    def dir_names(in_path):
+    def get_old_asset_data(self):
+        out = dict()
         try:
-            out = dict()
-            out["info_folder"] = in_path + "/" + INFO_FOLDER
-            out["content_folder"] = in_path + "/" + CONTENT_FOLDER
-            out["gallery_folder"] = in_path + "/" + GALLERY_FOLDER
-            out["asset_json"] = in_path + "/" + INFO_FOLDER + "/data.txt"
-            out["icon"] = in_path + "/" + INFO_FOLDER + "/icon.png"
-            return out
+            out["asset_id"], out["name"], out["path"] = self.Controller.Models.find_asset(id=self.asset_id)
+            logger.debug(" executed")
         except Exception as message:
             logger.error(message)
-            return False
+        return out
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+        if self.edit_mode and name != self.old_asset_data["name"]:
+            try:
+                # rename folders
+                new_asset_path = rename_path_list(self.old_asset_data["name"], name, [self.old_asset_data["path"]])[0]
+                self.scenes = rename_path_list(self.old_asset_data["name"], name, self.scenes)
+                self.gallery = rename_path_list(self.old_asset_data["name"], name, self.gallery)
+
+                os.rename(self.old_asset_data["path"], new_asset_path)
+                self.path = new_asset_path
+                # bd path edit
+                self.Controller.Models.edit_db_asset(**{"name": name,
+                                                        "asset_id": self.old_asset_data["asset_id"],
+                                                        "path": new_asset_path})
+                # edit info
+                asset_json = self.dir_names(new_asset_path)["asset_json"]
+                asset_data = self.info_file(asset_json)
+                asset_data["name"] = self.name
+                self.write_info_file(asset_json, asset_data)
+
+                logger.debug(" executed")
+            except Exception as message:
+                logger.error(message)
+                return None
 
     @property
     def path(self):
@@ -97,13 +133,13 @@ class Asset:
         self.write_info_file(self.asset_json, self.asset_data())
         self.Controller.Models.edit_db_asset(**self.asset_data())
 
-        self.verification_and_copying_files(self.scenes, self.content_folder)
-        self.verification_and_copying_files(self.gallery, self.gallery_folder)
+        self.copy_files(self.scenes, self.content_folder)
+        self.copy_files(self.gallery, self.gallery_folder)
 
         self.Controller.refresh_ui()
         self.Controller.ui.status_message("Asset " + self.name + " edited successfully!", )
 
-    def verification_and_copying_files(self, source_files, destination_folder):
+    def copy_files(self, source_files, destination_folder):
         destination_files = [destination_folder + "/" + x for x in os.listdir(destination_folder)]
         # if the file is not in the destination folder, copy it there
         while source_files:
@@ -121,10 +157,10 @@ class Asset:
                 logger.error(message)
 
     def create(self):
-        if not self.Controller.Models.find_asset_by_name(self.name):
+        if not self.Controller.Models.find_asset(name=self.name):
             try:
-                self.verification_and_copying_files(self.scenes, self.content_folder)
-                self.verification_and_copying_files(self.gallery, self.gallery_folder)
+                self.copy_files(self.scenes, self.content_folder)
+                self.copy_files(self.gallery, self.gallery_folder)
 
                 self.asset_id = self.Controller.Models.add_asset_to_db(**self.asset_data())
                 self.write_info_file(self.asset_json, self.asset_data())
@@ -171,7 +207,7 @@ class Asset:
             return False
 
     @staticmethod
-    def recognize_asset(path):
+    def recognize_asset(path, db_model):
         """
         gets information about an existing asset, paths from info and files from folders
         """
@@ -180,39 +216,39 @@ class Asset:
             asset_data = Asset.info_file(asset_json)
             asset_data["path"] = path
 
+            asset_data["asset_id"] = db_model.find_asset(path=path)[0]
+
             asset_content = os.listdir(Asset.dir_names(path)["content_folder"])
             asset_data["scenes"] = [Asset.dir_names(path)["content_folder"] + "/" + x for x in asset_content]
             gallery_content = os.listdir(Asset.dir_names(path)["gallery_folder"])
             asset_data["gallery"] = [Asset.dir_names(path)["gallery_folder"] + "/" + x for x in gallery_content]
-            logger.debug(" executed")
             logger.debug(json.dumps(asset_data))
             return asset_data
         except Exception as message:
             logger.error(message)
             return False
 
-    def delete_asset(self):
+    @staticmethod
+    def delete_asset(name, path):
         try:
             del_folder = get_library_path() + "/" + DELETED_ASSET_FOLDER
             if not os.path.exists(del_folder):
                 os.mkdir(del_folder)
             time = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M")
-            os.rename(self.path, self.path + "_" + time)
-            shutil.move(self.path + "_" + time, del_folder)
-            logger.debug("Asset " + self.name + " deleted successfully")
-            self.Controller.ui.status_message("Asset " + self.name + " deleted successfully")
+            os.rename(path, path + "_" + time)
+            shutil.move(path + "_" + time, del_folder)
+            logger.debug("Asset " + name + " deleted successfully")
+            return True
         except Exception as message:
             logger.error(message)
-            self.Controller.ui.status_message("An error occurred and the asset was not correctly deleted",
-                                              state="ERROR")
+            return False
 
     def asset_data(self):
-
         asset_data = dict()
         asset_data["name"] = self.name
         asset_data["path"] = self.path
         asset_data["asset_id"] = self.asset_id
-        asset_data["icon"] = self.__icon
+        asset_data["icon"] = self.icon
         asset_data["tags"] = self.tags
         asset_data["description"] = self.description
         asset_data["content_folder"] = self.content_folder
@@ -221,6 +257,20 @@ class Asset:
         asset_data["gallery"] = self.gallery
 
         return asset_data
+
+    @staticmethod
+    def dir_names(in_path):
+        try:
+            out = dict()
+            out["info_folder"] = in_path + "/" + INFO_FOLDER
+            out["content_folder"] = in_path + "/" + CONTENT_FOLDER
+            out["gallery_folder"] = in_path + "/" + GALLERY_FOLDER
+            out["asset_json"] = in_path + "/" + INFO_FOLDER + "/data.txt"
+            out["icon"] = in_path + "/" + INFO_FOLDER + "/icon.png"
+            return out
+        except Exception as message:
+            logger.error(message)
+            return False
 
 
 if __name__ == '__main__':
