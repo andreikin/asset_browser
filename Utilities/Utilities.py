67fcb3a6690bae6c76d2d7d68ba47e8ab408f6dd
Andrey Belyaev <andreikin@mail.ru>
2022-02-25 12:13:08 +0300

    add rename_scenes function

--- Utilities/Utilities.py
+++ Utilities/Utilities.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication
 
 # from Utilities.Logging import logger
+from Utilities.Logging import logger
 from settings import IMAGE_PREVIEW_SUFFIX, DROP_MENU_WIDTH, SFX
 
 
@@ -79,15 +80,6 @@ def create_preview_images(**kwargs):
     return out
 
 
-def rename_path_list(old_name, new_name, path_list):
-    """
-    renames multiple paths when changing the name of an asset
-    """
-    out = []
-    for path in path_list:
-        out.append(path.replace(old_name + SFX, new_name + SFX))
-    return out
-
 
 def remove_non_unique_tags(tags_list):
     out = []
@@ -97,15 +89,41 @@ def remove_non_unique_tags(tags_list):
     return out
 
 
-if __name__ == '__main__':
-    import sys
+def rename_scenes(name, path):
+    try:
+        scenes_list = os.listdir(path)
+        numbers = {os.path.splitext(x)[-1]: 0 for x in scenes_list}
+        name += SFX
+        path += "/"
+        for ext in numbers:
+            files = [x for x in scenes_list if ext == os.path.splitext(x)[-1]]
+            if len(files) == 1:
+                os.rename(path + files[0], path + name + ext)
+            else:
+                i = 1
+                for file in files:
+                    num = '_v{0:02d}'.format(i)
+                    os.rename(path + file, path + name + num + ext)
+                    i += 1
+    except Exception as message:
+        logger.error(message)
+        pass
 
-    # app = QApplication(sys.argv)
-    # path = "U:/AssetStorage/asset_browser/Characters/Human/Torso/Girl_Torso_03_ast/gallery"
-    #
-    # old_name = "Girl_Torso_03"
-    # new_name = "AssetSt"
-    #
-    # p_list = rename_path_list(old_name, new_name, [path, ])[0]
-    print(remove_non_unique_tags(["Aa", "Tags_list", "aa", "ss"]))
-    # sys.exit(app.exec_())
+
+def rename_path_list(old_name, new_name, path_list):
+    """
+    renames multiple paths when changing the name of an asset
+    """
+    out = []
+    for path in path_list:
+        out.append(path.replace("/" + old_name + SFX + "/", "/" + new_name + SFX + "/"))
+    return out
+
+
+if __name__ == '__main__':
+    name = "apple"
+    path = "C:/Users/avbeliaev/Desktop/Backpack_03_ast/content"
+
+    rename_scenes(name, path)
+    # for i in range(19 ):
+    #     print('{0:02d}'.format(i))
